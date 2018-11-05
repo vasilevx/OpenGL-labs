bedcde7fbe1c23d15b9756b0af891d413bb5953f
Nick <barmatun101@mail.ru>
2018-11-05 03:31:24 +0300

    Вторая лаба(1-3)

--- OpenGL-2/src/main.cpp
+++ OpenGL-2/src/main.cpp
@@ -8,14 +8,57 @@ GLvoid DrawGLScene() {
 	/*Очистка экрана*/
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glPointSize(2); //размер точки
+	/*Включаем сглаживание*/
+	glEnable(GL_POINT_SMOOTH);
+	glEnable(GL_LINE_SMOOTH);
+
+	/*Точки в вершинах*/
+	glPointSize(10);
 	glBegin(GL_POINTS);
-	glColor3d(1, 0, 0);
-	glVertex3d(-0.45, -0.4, 0); // первая точка
-	glColor3d(0, 1, 0);
-	glVertex3d(0.4, 0.4, 0); // вторая точка
-	glColor3d(0, 0, 1);
-	glVertex3d(-0.35, 0.4, 0); // третья точка
+		glColor3f(1, 0, 0);
+		glVertex2f(-0.95, 0.95);
+		glColor3f(0, 1, 0);
+		glVertex2f(-0.65, 0.95);
+		glColor3f(0, 0, 1);
+		glVertex2f(-0.95, 0.65);
+		glColor3f(1, 1, 1);
+		glVertex2f(-0.65, 0.65);
+	glEnd();
+
+	/*n-угольник линиями(solid)*/
+	glLineWidth(3);
+	glLineStipple(1, 0x0F0F);
+	glBegin(GL_LINES);
+		glVertex2f(-0.6, 0.95);
+		glVertex2f(-0.6, 0.65);
+		glVertex2f(-0.3, 0.95);
+		glVertex2f(-0.3, 0.65);
+	glEnd();
+
+	/*n-угольник линиями(dashed)*/
+	glEnable(GL_LINE_STIPPLE);
+	glLineWidth(10);
+	glLineStipple(1, 0x0F0F);
+	glBegin(GL_LINES);
+		glColor3f(0.5, 0.3, 0.3);
+		glVertex2f(-0.6, 0.95);
+		glVertex2f(-0.3, 0.95);
+		glVertex2f(-0.6, 0.65);
+		glVertex2f(-0.3, 0.65);
+	glEnd();
+	glDisable(GL_LINE_STIPPLE);
+
+	/*Рис.1 ломаная линия*/
+	glLineWidth(1);
+	glBegin(GL_LINE_STRIP);
+		glColor3f(1, 1, 1);
+		glVertex2f(-0.2, 0.95);
+		glVertex2f(0.4, 0.95);
+		glVertex2f(0, 0.85);
+		glVertex2f(0.5, 0.75);
+		glVertex2f(-0.2, 0.75);
+		glVertex2f(0, 0.6);
+		glVertex2f(-0.2, 0.45);
 	glEnd();
 
 	/*Копирование скрытого буфера на экран*/
@@ -31,7 +74,7 @@ GLvoid ReSizeGLScene(GLsizei width, GLsizei height) {
 
 void KeyboardHandler(unsigned char key, int x, int y) {
 	if (key == 27)
-		exit(0);
+		glutLeaveMainLoop();
 }
 
 int main(int argc, char ** argv) {
@@ -40,7 +83,7 @@ int main(int argc, char ** argv) {
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 	glutInitWindowSize(640, 480);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("OpenGL#1");
+	glutCreateWindow("OpenGL#2");
 	glutDisplayFunc(DrawGLScene);
 	glutKeyboardFunc(KeyboardHandler);
 	glutReshapeFunc(ReSizeGLScene);
